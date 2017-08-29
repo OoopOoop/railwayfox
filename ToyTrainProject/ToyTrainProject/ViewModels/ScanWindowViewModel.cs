8a30710f34159df7fb9beb5b0e873babbf9748f7
OoopOoop <OoopOoop@users.noreply.github.com>
2017-08-29 21:47:27 +0100

    setup main page

---
+++ ToyTrainProject/ToyTrainProject/ViewModels/ScanWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using GalaSoft.MvvmLight.Command;
+
+namespace ToyTrainProject.ViewModels
+{
+    public class ScanWindowViewModel:ViewModelBase
+    {
+        public ScanWindowViewModel()
+        {
+            
+        }
+        private RelayCommand _testClickCommand;
+        public RelayCommand TestClickCommand => _testClickCommand ?? (_testClickCommand = new RelayCommand(testMethod));
+
+        private void testMethod()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
