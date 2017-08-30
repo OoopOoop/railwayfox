c5cdd5be97d1400e95b914bc035ea8467d10b850
OoopOoop <OoopOoop@users.noreply.github.com>
2017-08-30 23:05:15 +0100

    add grid to the main page

---
+++ ToyTrainProject/ToyTrainProject/Webcam.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace ToyTrainProject
+{
+    /// <summary>
+    /// Interaction logic for Webcam.xaml
+    /// </summary>
+    public partial class Webcam : UserControl
+    {
+        public Webcam()
+        {
+            InitializeComponent();
+        }
+    }
+}
