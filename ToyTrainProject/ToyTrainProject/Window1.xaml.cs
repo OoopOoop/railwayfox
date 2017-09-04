9d86c2cc77faf341ab4d2e790d1e4d1bcc6fc607
OoopOoop <OoopOoop@users.noreply.github.com>
2017-09-04 23:06:02 +0100

    create camera control

--- ToyTrainProject/ToyTrainProject/Window1.xaml.cs
+++ ToyTrainProject/ToyTrainProject/Window1.xaml.cs
@@ -3,7 +3,6 @@ using AForge.Video.DirectShow;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
-using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -16,24 +15,24 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace ToyTrainProject
 {
     /// <summary>
-    /// Interaction logic for UserControl1.xaml
+    /// Interaction logic for Window1.xaml
     /// </summary>
-    public partial class Webcam : UserControl
+    public partial class Window1 : Window
     {
         private BitmapImage latestFrame;
         VideoCaptureDevice LocalWebCam;
         public FilterInfoCollection LocalWebCamsCollection;
         Action<BitmapImage> captureImage;
 
-        public Webcam()
+        public Window1()
         {
             InitializeComponent();
+            this.DataContext = this;
 
             Loaded += CameraWindow_Loaded;
             Unloaded += CameraWindow_Unloaded;
@@ -41,27 +40,31 @@ namespace ToyTrainProject
 
         void Cam_NewFrame(object sender, NewFrameEventArgs eventArgs)
         {
-            try
-            {
-                System.Drawing.Image img = (Bitmap)eventArgs.Frame.Clone();
-                MemoryStream ms = new MemoryStream();
-                img.Save(ms, ImageFormat.Bmp);
-                ms.Seek(0, SeekOrigin.Begin);
-                BitmapImage bi = new BitmapImage();
-                bi.BeginInit();
-                bi.StreamSource = ms;
-                bi.EndInit();
-                bi.Freeze();
-                this.latestFrame = bi;
-                Dispatcher.BeginInvoke(new ThreadStart(delegate
-                {
-                    videoWindow.Source = bi;
-                }));
-            }
-            catch (Exception ex)
-            {
+            //try
+            //{
+            //    System.Drawing.Image img = (Bitmap)eventArgs.Frame.Clone();
+            //    MemoryStream ms = new MemoryStream();
+            //    img.Save(ms, System.Drawing.Imaging.ImageFormat.Bmp);
+            //    ms.Seek(0, SeekOrigin.Begin);
+            //    BitmapImage bi = new BitmapImage();
+            //    bi.BeginInit();
+            //    bi.StreamSource = ms;
+            //    bi.EndInit();
+            //    bi.Freeze();
+            //    this.latestFrame = bi;
+            //    Dispatcher.BeginInvoke(new ThreadStart(delegate
+            //    {
+            //        videoWindow.Source = bi;
+            //    }));
+            //}
+            //catch (Exception ex)
+            //{
 
-            }
+            //}
+
+            System.Drawing.Image img = (Bitmap)eventArgs.Frame.Clone();
+
+           
         }
 
         private void CameraWindow_Loaded(object sender, RoutedEventArgs e)
@@ -89,6 +92,5 @@ namespace ToyTrainProject
             }
         }
 
-     
     }
 }
