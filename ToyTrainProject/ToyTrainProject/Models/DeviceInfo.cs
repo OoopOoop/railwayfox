51d7cf130740d2157c55a4f761dfaf4f8f016087
OoopOoop <OoopOoop@users.noreply.github.com>
2017-09-06 23:01:51 +0100

    change camera control

---
+++ ToyTrainProject/ToyTrainProject/Models/DeviceInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace ToyTrainProject.Models
+{
+    public class DeviceInfo
+    {
+        public string DisplayName { get; set; }
+        public string UsbString { get; set; }
+    }
+}
--- ToyTrainProject/ToyTrainProject/ViewModels/ScanWindowViewModel.cs
+++ ToyTrainProject/ToyTrainProject/ViewModels/ScanWindowViewModel.cs
@@ -1,32 +1,37 @@
-﻿using System;
-using GalaSoft.MvvmLight.Command;
-using System.Drawing;
-using System.Windows.Media.Imaging;
-using System.Windows.Media;
+﻿using GalaSoft.MvvmLight.Command;
+using ToyTrainProject.Models;
 
 namespace ToyTrainProject.ViewModels
 {
     public class ScanWindowViewModel:ViewModelBase
     {
+        private DeviceInfo _selectedDevice;
+        public DeviceInfo SelectedDevice
+        {
+            get { return _selectedDevice; }
+            set { _selectedDevice = value; OnPropertyChanged(); }
+        }
+
+        private string _cameraUsbId;
+        public string CameraUsbId
+        {
+            get { return _cameraUsbId; }
+            set { _cameraUsbId = value; OnPropertyChanged(); }
+        }
+
+
         public ScanWindowViewModel()
         {
-            VideoSourcee = null;
+           
         }
+
         private RelayCommand _testClickCommand;
         public RelayCommand TestClickCommand => _testClickCommand ?? (_testClickCommand = new RelayCommand(testMethod));
 
         private void testMethod()
         {
-            var test = VideoSourcee;
+           
         }
-
-        private ImageSource _videoSourcee;
-        public ImageSource VideoSourcee
-        {
-            get { return _videoSourcee; }
-            set { _videoSourcee = value;
-                this.OnPropertyChanged(); }
-        }
-
+        
     }
 }
