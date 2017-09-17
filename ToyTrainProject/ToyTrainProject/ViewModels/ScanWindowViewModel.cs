bc9dde84515805ab7784808d78e6e22f05a96cd6
zabikol <zabikol-git@yahoo.com>
2017-09-17 19:53:35 +0100

    Simplify ScanWindowViewModel
    Use newtonsoft for pretty json and remove obsolote members

--- ToyTrainProject/ToyTrainProject/ViewModels/ScanWindowViewModel.cs
+++ ToyTrainProject/ToyTrainProject/ViewModels/ScanWindowViewModel.cs
@@ -3,34 +3,20 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Text;
 using System.Windows.Media;
 using GalaSoft.MvvmLight.Command;
 using ToyTrainProject.Models;
-using static ToyTrainProject.Shared.AppConfiguration;
 
 namespace ToyTrainProject.ViewModels
 {
     public class ScanWindowViewModel : ViewModelBase
     {
-        private const string TemporaryImagePath = "";
-
         private DeviceInfo _selectedDevice;
 
         public DeviceInfo SelectedDevice
         {
             get { return _selectedDevice; }
             set { _selectedDevice = value; OnPropertyChanged(); }
-        }
-
-        private string _cameraUsbId;
-
-        public string CameraUsbId
-        {
-            get { return _cameraUsbId; }
-            set { _cameraUsbId = value; OnPropertyChanged(); }
         }
 
         private string _responseText;
@@ -82,7 +68,8 @@ namespace ToyTrainProject.ViewModels
         private async void SubmitSnapShot()
         {
             var content = await new ComputerVision_AnalyseImage().callService(SnapshotBitmap);
-            ResponseText = JsonPrettyPrint(content);
+            dynamic parsedJson = Newtonsoft.Json.JsonConvert.DeserializeObject(content);
+            ResponseText = Newtonsoft.Json.JsonConvert.SerializeObject(parsedJson, Newtonsoft.Json.Formatting.Indented);
         }
 
         public static ImageSource ConvertToImageSource(Bitmap bitmap)
@@ -94,73 +81,6 @@ namespace ToyTrainProject.ViewModels
                 var snapshotBytes = memoryStream.ToArray();
                 return (ImageSource)imageSourceConverter.ConvertFrom(snapshotBytes); ;
             }
-        }
-
-        private static string JsonPrettyPrint(string json)
-        {
-            if (string.IsNullOrEmpty(json))
-                return string.Empty;
-
-            json = json.Replace(Environment.NewLine, "").Replace("\t", "");
-
-            StringBuilder sb = new StringBuilder();
-            bool quote = false;
-            bool ignore = false;
-            int offset = 0;
-            int indentLength = 3;
-
-            foreach (char ch in json)
-            {
-                switch (ch)
-                {
-                    case '"':
-                        if (!ignore) quote = !quote;
-                        break;
-
-                    case '\'':
-                        if (quote) ignore = !ignore;
-                        break;
-                }
-
-                if (quote)
-                    sb.Append(ch);
-                else
-                {
-                    switch (ch)
-                    {
-                        case '{':
-                        case '[':
-                            sb.Append(ch);
-                            sb.Append(Environment.NewLine);
-                            sb.Append(new string(' ', ++offset * indentLength));
-                            break;
-
-                        case '}':
-                        case ']':
-                            sb.Append(Environment.NewLine);
-                            sb.Append(new string(' ', --offset * indentLength));
-                            sb.Append(ch);
-                            break;
-
-                        case ',':
-                            sb.Append(ch);
-                            sb.Append(Environment.NewLine);
-                            sb.Append(new string(' ', offset * indentLength));
-                            break;
-
-                        case ':':
-                            sb.Append(ch);
-                            sb.Append(' ');
-                            break;
-
-                        default:
-                            if (ch != ' ') sb.Append(ch);
-                            break;
-                    }
-                }
-            }
-
-            return sb.ToString().Trim();
         }
     }
 }
