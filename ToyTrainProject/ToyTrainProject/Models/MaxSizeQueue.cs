17315d360bdd8f885d49e2eeeaa642e5d320611f
zabikol <zabikol-git@yahoo.com>
2017-09-17 18:39:48 +0100

    Add limited queue class
    Queue class that holds a maximum number of objects in the queue - oldest
    items are pushed out

---
+++ ToyTrainProject/ToyTrainProject/Models/MaxSizeQueue.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Concurrent;
+
+namespace ToyTrainProject.Models
+{
+    public class MaxSizeQueue<T>
+    {
+        private readonly object privateLockObject = new object();
+
+        private readonly ConcurrentQueue<T> queue = new ConcurrentQueue<T>();
+
+        public int Size { get; }
+
+        public MaxSizeQueue(int size)
+        {
+            Size = size;
+        }
+
+        public void Enqueue(T obj)
+        {
+            queue.Enqueue(obj);
+
+            lock (privateLockObject)
+            {
+                while (queue.Count > Size)
+                {
+                    T outObj;
+                    queue.TryDequeue(out outObj);
+                }
+            }
+        }
+    }
+}
